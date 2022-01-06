59c72dc5a7dbbba3ec56b7e589c839dcf18de67f
João Vitor da Silva <silva.joao1@outlook.com.br>
2022-01-05 23:31:59 -0300

    feat: documentation

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import "express-async-errors";
+import swaggerUi from "swagger-ui-express";
 
+import swaggerFile from "./config/swagger.json";
 import ExpectionHandler from "./middlewares/expectionHandler";
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use("/users", usersRoutes);
 
